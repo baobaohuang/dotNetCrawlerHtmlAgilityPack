a9874e996a7223db97740343fe5e5f6a40e2b123
baobaohuang <huangnf95@gmail.com>
2015-08-24 17:23:39 +0800

    add TW50
    add tw50 price high low and percent of the price float

--- crawler2/Program.cs
+++ crawler2/Program.cs
@@ -22,6 +22,10 @@ namespace crawler2
                 StockID.Add("9904");
                 StockID.ForEach(getdata);                
             }
+            else if (str=="50")
+            {
+                getTaiwan50();
+            }
             else
             {
                 getdata(str);
@@ -71,5 +75,74 @@ namespace crawler2
             ms.Close();            
 
         }
+
+        static void getTaiwan50()
+        {
+            // 下載 Yahoo 奇摩股市資料 (範例為 2317 鴻海) 
+            WebClient client = new WebClient();
+            MemoryStream ms = new MemoryStream(client.DownloadData(
+        "https://tw.stock.yahoo.com/s/list2.php?c=%A5x%C6W50"));
+
+            // 使用預設編碼讀入 HTML 
+            HtmlDocument doc = new HtmlDocument();
+            doc.Load(ms, Encoding.Default);
+
+            // 裝載第一層查詢結果 
+            HtmlDocument docStockContext = new HtmlDocument();
+
+            docStockContext.LoadHtml(doc.DocumentNode.SelectSingleNode(
+"/html[1]/body[1]/center[1]/table[2]/tr[2]/td[1]/table[1]/tr[1]/td[1]/table[1]").InnerHtml);
+
+            // 取得個股標頭 
+            HtmlNodeCollection nodeHeaders =
+         docStockContext.DocumentNode.SelectNodes("./tr[1]/th");
+
+            // 取得個股數值
+            stock[] stocks = new stock[50];
+            int i = 0;
+            for (int stock = 2; stock < 52; stock++)
+            {
+                string nodeNumber = "./tr[" + stock + "]";
+           
+                string[] values = docStockContext.DocumentNode.SelectSingleNode(nodeNumber).InnerText.Trim().Split('\n');
+                
+                // 輸出資料
+                decimal a =  Convert.ToDecimal(values[10].Trim());
+                decimal b =  Convert.ToDecimal(values[11].Trim());
+                decimal c =  Convert.ToDecimal(values[10].Trim()) - Convert.ToDecimal(values[11].Trim());
+                stocks[i] = new stock { Name = values[0].Trim(), HighPrice = a, LowPrice = b, PriceDrop = c, Percent = decimal.Round(c * 100 / a,1) };
+                Console.Write(stocks[i].Name);
+                Console.Write(stocks[i].PriceDrop);
+                Console.Write(" " + stocks[i].Percent +"%");
+                Console.Write("\n");
+            }
+
+
+            //IEnumerable<stock> query = stocks.OrderByDescending( x =>(x).Percent);
+            ////IEnumerable<stock> query = from e in stocks orderby e.Percent descending select e;
+            ////var query = from e in stocks orderby e.Percent select e;
+            ////var query = stocks.OrderBy(x => x.Percent);
+            //foreach (stock output in query )
+            //{
+            //    Console.Write(output.Name);
+            //    Console.Write(output.PriceDrop);
+            //    Console.Write(" " + output.Percent);
+            //    Console.Write("\n");
+            //}
+
+            doc = null;
+            docStockContext = null;
+            client = null;
+            ms.Close();   
+        }
+    }
+    
+    class stock
+    {
+        public string Name { get; set; }
+        public decimal HighPrice { get; set; }
+        public decimal LowPrice { get; set; }
+        public decimal PriceDrop { get; set; }
+        public decimal Percent { get; set; }
     }
 }
